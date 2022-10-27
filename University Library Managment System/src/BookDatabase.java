2b5394b28a249fd87962bbfcf7a8819a69a12665
karimelkazaz <es-KarimAshraf2025@alexu.edu.eg>
2022-10-27 17:57:01 +0200

    some basics

--- University Library Managment System/src/BookDatabase.java
+++ University Library Managment System/src/BookDatabase.java
@@ -1,2 +1,42 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BookDatabase {
+
+    private ArrayList<Book> records = new ArrayList<Book>();
+    private String fileName;
+
+    private File recordsFile;
+
+    public BookDatabase (String fileName) {
+
+        this.fileName = fileName;
+    }
+
+    public void readFromFile() {
+
+        recordsFile = new File(fileName);
+
+        // reads all the records of the books from the file and store them in the record array
+        try {
+
+            Scanner scanner = new Scanner(recordsFile);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                Book book = new Book(parts[0], parts[1], parts[2], parts[3], Integer.parseInt(parts[4]));
+                records.add(book);
+            }
+
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error no file found");
+        }
+    }
+
+
+
+
 }
--- University Library Managment System/src/StudentBook.java
+++ University Library Managment System/src/StudentBook.java
@@ -1,2 +1,41 @@
+import java.time.LocalDate;
+
 public class StudentBook {
+
+    private String studentId;
+    private String bookId;
+    private LocalDate borrowDate;
+
+    public StudentBook(String studentId, String bookId, LocalDate borrowDate) {
+
+        this.studentId = studentId;
+        this.bookId = bookId;
+        this.borrowDate = borrowDate;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public String lineRepresentation() {
+
+        String line = studentId + "," + bookId + "," + borrowDate;
+        return line;
+    }
+
+    public String getSearchKey() {
+
+        String key = studentId + "," + bookId;
+        return key;
+    }
+
+
 }
