38096bf14fb8ecab94817f947f717747cfe8bec1
OssamaTammam <ossamatammam@gmail.com>
2022-10-27 13:27:43 +0200

    Update LibrarianUser.java

--- University Library Managment System/src/LibrarianUser.java
+++ University Library Managment System/src/LibrarianUser.java
@@ -1,4 +1,29 @@
 public class LibrarianUser {
 
+    private String librarianId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    public LibrarianUser(String librarianId, String name, String email, String address, String phoneNumber) {
+
+        this.librarianId = librarianId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String lineRepresentation() {
+
+        return String.format("%s,%s,%s,%s,%s", librarianId, name, email, address, phoneNumber);
+    }
+
+    public String getSearchKey() {
+
+        return librarianId;
+    }
+
 
 }
