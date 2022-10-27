7a6246dccf330b02a2dc63d83e33a5ac4b4c31b6
karimelkazaz <es-KarimAshraf2025@alexu.edu.eg>
2022-10-27 16:07:08 +0200

    1st book class

--- University Library Managment System/src/Book.java
+++ University Library Managment System/src/Book.java
@@ -1,2 +1,37 @@
 public class Book {
+
+    private String bookId;
+    private String title;
+    private String authorName;
+    private String publisherName;
+    private int quantity;
+
+    public Book(String bookId, String title, String authorName, String publisherName, int quantity) {
+
+        this.bookId = bookId;
+        this.title = title;
+        this.authorName = authorName;
+        this.publisherName = publisherName;
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String lineRepresentation() {
+
+        String line = bookId + "," + title + "," + authorName + "," + publisherName + "," + quantity;
+        return line;
+    }
+
+    public String getSearchKey() {
+
+        return bookId;
+    }
+
 }
