6657df22592a37cc64012c22dd880c37b23575a6
OssamaTammam <ossamatammam@gmail.com>
2022-11-10 18:33:08 +0200

    Developed GUI

---
+++ University Library Managment System/src/frontend/Node.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package frontend;
+
+/**
+ *
+ * @author Ossama
+ */
+public interface Node {
+    
+    public void setParentNode(Node parent);
+    public Node getParentNode();
+}
